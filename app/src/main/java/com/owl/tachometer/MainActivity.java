b94373fac67271cf054f7e4215aaee14a77b3593
owl <intmainreturn00@gmail.com>
2016-06-17 01:46:49 +0300

    android studio custom view sample generated. add invalidate(), requestLayout(); to it. (seems like android studio defaults can't properly generate setters for custom views.

--- app/src/main/java/com/owl/tachometer/MainActivity.java
+++ app/src/main/java/com/owl/tachometer/MainActivity.java
@@ -1,14 +1,29 @@
 package com.owl.tachometer;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
+
+    private Tachometer tachometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tachometer = (Tachometer) findViewById(R.id.tachometer);
+    }
+
+    public void fun(View v) {
+        tachometer.setExampleString("qqqqqqqqqqqqqqqqqq");
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        tachometer.setExampleColor(color);
     }
 }
