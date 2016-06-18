225d28993b9ce2c368419769faa77d7dff41e57a
owl <intmainreturn00@gmail.com>
2016-06-19 00:26:24 +0300

    service acceleration for values

--- app/src/main/java/com/owl/tachometer/TachometerService.java
+++ app/src/main/java/com/owl/tachometer/TachometerService.java
@@ -51,6 +51,8 @@ public class TachometerService extends Service {
                     release();
                     push();
                     release();
+                    release();
+                    push();
                     push();
                     release();
 
@@ -63,26 +65,32 @@ public class TachometerService extends Service {
         }
 
         private void push() throws InterruptedException {
-            for (int i = 0; i < 6 && running; ++i) {
-                rps += 400;
+            float acceleration = 1f;
+            for (int i = 0; i < 4 && running; ++i) {
+                rps += 500 * acceleration;
                 sendValue(rps);
                 delay();
+                acceleration += 0.3;
             }
         }
 
         private void release() throws InterruptedException {
+            float acceleration = 2f;
             for (int i = 0; i < 3 && running; ++i) {
-                rps -= 200;
+                rps -= 300 * acceleration;
                 sendValue(rps);
                 delay();
+                acceleration += 0.3;
             }
         }
 
         private void leave() throws InterruptedException {
+            float acceleration = 1f;
             while (rps > 1000 && running) {
-                rps -= 200;
+                rps -= 200 * acceleration;
                 sendValue(rps);
                 delay();
+                acceleration += 0.3;
             }
             rps = 1000;
         }
